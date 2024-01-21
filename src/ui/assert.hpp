de64f752ce71ab0ca1ed4cb6946bb5b9a816ab63
ajaysreedhar <sree.ajay468@gmail.com>
2024-01-21 23:55:35 +0530

    add: Macros for SDL handle

---
+++ src/ui/assert.hpp
@@ -0,0 +1,32 @@
+/**
+ * assert.hpp - Various macros for assertion.
+ * ------------------------------------------------------------------------
+ *
+ * Copyright (c) 2024-present Ajay Sreedhar
+ * 
+ * Licensed under the MIT License.
+ * Please see the LICENSE file located in the root directory.
+ *
+ * ========================================================================
+ */
+
+#ifndef BMX_UI_ASSERT_H
+#define BMX_UI_ASSERT_H
+
+#include <SDL2/SDL_error.h>
+#include "except.hpp"
+
+#define BMX_ASSERT_SDL_HANDLE(handle, code) \
+if (handle == nullptr) {                    \  
+    const char* message = SDL_GetError();   \
+    throw bmx::UIError(message, code);      \
+}
+
+
+#define BMX_ASSERT_SDL_RESULT(result)       \
+if (result <= -1) {                         \
+    const char* message = SDL_GetError();   \
+    throw bmx::UIError(message, result);    \
+}
+
+#endif // BMX_UI_ASSERT_H
