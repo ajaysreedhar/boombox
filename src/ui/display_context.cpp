cb159f574b76b897fac0d8e0e3818c29f72b978e
ajaysreedhar <sree.ajay468@gmail.com>
2024-01-25 23:33:26 +0530

    change: Rename SDL polling to wait event

--- src/ui/display_context.cpp
+++ src/ui/display_context.cpp
@@ -68,7 +68,7 @@ bmx::DisplayContext::UIEvent bmx::DisplayContext::pollEvent() {
     SDL_Event sdl_event {};
     bmx::DisplayContext::UIEvent bmx_event {bmx::DisplayContext::UIEvent::UNKNOWN};
 
-    SDL_PollEvent(&sdl_event);
+    SDL_WaitEvent(&sdl_event);
 
     if (sdl_event.type == SDL_QUIT) {
         bmx_event.kind = bmx::DisplayContext::UIEvent::PROCESS_QUIT;
