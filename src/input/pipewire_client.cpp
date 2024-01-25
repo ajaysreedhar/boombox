744cd1afd3dbce543d95ca7f40208b50b568e121
ajaysreedhar <sree.ajay468@gmail.com>
2024-01-25 23:31:49 +0530

    TODO: Fix pipewire stream error

--- src/input/pipewire_client.cpp
+++ src/input/pipewire_client.cpp
@@ -1,7 +1,82 @@
+#include <cmath>
+#include <thread>
+#include <csignal>
+#include "platform/logger.hpp"
 #include "except.hpp"
 #include "pipewire_client.hpp"
 
 bool bmx::PipewireClient::s_isInitialised = false;
+
+void bmx::PipewireClient::onStreamChangedCb(void* bundle, uint32_t id, const struct spa_pod* pod_param) {
+    auto payload = reinterpret_cast<bmx::InputBundle*>(bundle);
+
+    if (pod_param == nullptr || id != SPA_PARAM_Format) {
+        return void();
+    }
+
+    if (spa_format_parse(pod_param, &payload->format.media_type, &payload->format.media_subtype) < 0) {
+        return void();
+    }
+
+    if (payload->format.media_type != SPA_MEDIA_TYPE_audio ||
+        payload->format.media_subtype != SPA_MEDIA_SUBTYPE_raw) {
+        return void();
+    }
+
+    spa_format_audio_raw_parse(pod_param, &payload->format.info.raw);
+
+    bmx::Logger::info("Capturing rate", payload->format.info.raw.rate, "channels", payload->format.info.raw.channels);
+}
+
+void bmx::PipewireClient::onProcessHolderCb(void* bundle) {
+    auto payload = reinterpret_cast<bmx::InputBundle*>(bundle);
+
+    /*
+    if (*payload->running == false) {
+        std::thread::id thread_id = std::this_thread::get_id();
+        bmx::Logger::debug("Callback thread ", thread_id);
+    
+        int result = pw_main_loop_quit(payload->loop);
+        bmx::Logger::debug("PipewireClient: Halt signal recieved.", result);
+        
+        return void();
+    }
+    */
+
+    struct pw_buffer *b;
+    struct spa_buffer *buf;
+    float* samples, max;
+
+    uint32_t c, n, n_channels, n_samples, peak;
+
+    if ((b = pw_stream_dequeue_buffer(payload->stream)) == nullptr) {
+            pw_log_warn("out of buffers: %m");
+            return void();
+    }
+
+    buf = b->buffer;
+    if (buf->datas[0].data == nullptr) {
+        return void();
+    }
+
+    samples = (float*) buf->datas[0].data;
+
+    n_channels = payload->format.info.raw.channels;
+    n_samples = buf->datas[0].chunk->size / sizeof(float);
+
+    int peak_1, peak_2;
+
+    for (c = 0; c < n_channels; c++) {
+        max = 0.0f;
+        for (n = c; n < n_samples; n += n_channels) {
+            max = fmaxf(max, fabsf(samples[n]));
+        }
+
+        peak = SPA_CLAMP(max * 30, 0, 40);
+    }
+
+    pw_stream_queue_buffer(payload->stream, b);
+}
 
 void bmx::PipewireClient::initialise(int argc, char** argv) {
     pw_init(&argc, &argv);
@@ -24,21 +99,64 @@ bmx::PipewireClient* bmx::PipewireClient::factory() {
 }
 
 bmx::PipewireClient::PipewireClient() {
+    const struct spa_pod* pod_params[1];
+    struct spa_audio_info_raw audio_info {.format = SPA_AUDIO_FORMAT_F32};
+
     m_podBuilder = SPA_POD_BUILDER_INIT(m_buffer, sizeof(m_buffer));
     
-    m_pwLoop = pw_main_loop_new(nullptr);
+    m_pwLoop = m_payload.loop = pw_main_loop_new(nullptr);
     m_pwProps = pw_properties_new(PW_KEY_MEDIA_TYPE, "Audio",
         PW_KEY_MEDIA_CATEGORY, "Capture",
         PW_KEY_MEDIA_ROLE, "Music", nullptr);
 
-    // m_pwStream = pw_stream_new_simple(pw_main_loop_get_loop(m_pwLoop), "boombox", m_pwProps, );
+    m_pwEvents.param_changed = bmx::PipewireClient::onStreamChangedCb;
+    m_pwEvents.process = onProcessHolderCb;
+
+    m_payload.running = &m_isRunning;
+    printf("\n isRunning %u = %u v:%d\n", &m_isRunning, m_payload.running, *m_payload.running);
+    m_pwStream = m_payload.stream = pw_stream_new_simple(pw_main_loop_get_loop(m_pwLoop), "boombox", m_pwProps, &m_pwEvents, &m_payload);
+
+    pod_params[0] = spa_format_audio_raw_build(&m_podBuilder, SPA_PARAM_EnumFormat, &audio_info);
+
+    pw_stream_connect(m_pwStream,
+        PW_DIRECTION_INPUT,
+        PW_ID_ANY,
+        (pw_stream_flags)(PW_STREAM_FLAG_AUTOCONNECT |
+        PW_STREAM_FLAG_MAP_BUFFERS |
+        PW_STREAM_FLAG_RT_PROCESS),
+        pod_params, 1);
 }
 
 bmx::PipewireClient::~PipewireClient() {
-    pw_stream_destroy(m_pwStream);
     pw_main_loop_destroy(m_pwLoop);
+
+#if defined(BMX_MODE_DEBUG) && BMX_MODE_DEBUG == 1
+    bmx::Logger::debug("Pipewire client destroyed.");
+#endif // BMX_MODE_DEBUG
 }
 
 void bmx::PipewireClient::capture() {
+    m_isRunning = true;
+
+    std::thread::id thread_id = std::this_thread::get_id();
+    bmx::Logger::debug("Capture thread ", thread_id);
+
     pw_main_loop_run(m_pwLoop);
+    pw_stream_destroy(m_pwStream);
+
+#if defined(BMX_MODE_DEBUG) && BMX_MODE_DEBUG == 1
+    bmx::Logger::debug("Pipewire capture aborted.");
+#endif // BMX_MODE_DEBUG
+
+    m_isRunning = false;
+    return void();
+}
+
+void bmx::PipewireClient::halt() {
+    m_isRunning = false;
+    pw_main_loop_quit(m_pwLoop);
+}
+
+void bmx::PipewireClient::attachListener(void (*listener)(void *)) {
+    m_pwEvents.process = listener;
 }
--- src/input/pipewire_client.hpp
+++ src/input/pipewire_client.hpp
@@ -6,6 +6,18 @@
 
 namespace bmx {
 
+
+struct input_bundle {
+    struct pw_main_loop*    loop;
+    struct pw_stream*       stream;
+    struct spa_audio_info   format;
+    bool*                   running;
+};
+
+typedef struct input_bundle InputBundle;
+
+
+
 /**
  * @brief Client capturing audio from Pipewire interface.
  */
@@ -14,15 +26,23 @@ class PipewireClient {
 private: /* ===-=== Private members ===-=== */
     static bool s_isInitialised;
 
+    bool m_isRunning = false;
+
     uint8_t m_buffer[1024];
     
     struct pw_main_loop*    m_pwLoop;
     struct pw_properties*   m_pwProps;
     struct pw_stream*       m_pwStream;
+    struct pw_stream_events m_pwEvents {PW_VERSION_STREAM_EVENTS};
     
     struct spa_pod_builder  m_podBuilder;
 
+    bmx::InputBundle        m_payload;
+
     explicit PipewireClient();
+
+    static void onStreamChangedCb(void*, uint32_t, const struct spa_pod*);
+    static void onProcessHolderCb(void*);
 
 public: /* ===-=== Public members ===-=== */
     static void initialise(int, char**);
@@ -30,6 +50,8 @@ public: /* ===-=== Public members ===-=== */
     static PipewireClient* factory();
 
     void capture();
+    void halt();
+    void attachListener(void (*)(void *));
 
     ~PipewireClient();
 };
