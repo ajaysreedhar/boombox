6fd44e1f24c8d632d0ba5a510e94c9f93b386a8a
ajaysreedhar <sree.ajay468@gmail.com>
2024-01-28 23:47:56 +0530

    TODO: Add event observer prototype

---
+++ src/lib/event_manager.cpp
@@ -0,0 +1,11 @@
+/**
+ * event_manager.cpp - Event manager utility.
+ * ------------------------------------------------------------------------
+ *
+ * Copyright (c) 2024-present Ajay Sreedhar
+ * 
+ * Licensed under the MIT License.
+ * Please see the LICENSE file located in the root directory.
+ *
+ * ========================================================================
+ */
---
+++ src/lib/event_manager.hpp
@@ -0,0 +1,22 @@
+/**
+ * event_manager.hpp - Event manager utility.
+ * ------------------------------------------------------------------------
+ *
+ * Copyright (c) 2024-present Ajay Sreedhar
+ * 
+ * Licensed under the MIT License.
+ * Please see the LICENSE file located in the root directory.
+ *
+ * ========================================================================
+ */
+
+#ifndef BMX_LIB_EVENT_MANAGER_H
+#define BMX_LIB_EVENT_MANAGER_H
+
+namespace bmx {
+
+
+    
+}
+
+#endif // BMX_LIB_EVENT_MANAGER_H
