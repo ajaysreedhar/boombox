9866fbe848aa244c0b59a02c52b50335b0f1b0d8
ajaysreedhar <sree.ajay468@gmail.com>
2024-02-02 23:41:59 +0530

    remove: Unused references from audio payload

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,15 @@
+/**
+ * main.cpp - Execution begins here.
+ * ------------------------------------------------------------------------
+ *
+ * Copyright (c) 2024-present Ajay Sreedhar
+ * 
+ * Licensed under the MIT License.
+ * Please see the LICENSE file located in the root directory.
+ *
+ * ========================================================================
+ */
+
 #include <cstdlib>
 #include <thread>
 #include "platform/logger.hpp"
